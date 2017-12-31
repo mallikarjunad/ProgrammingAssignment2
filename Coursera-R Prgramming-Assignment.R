dd313f658222e51e365565439ca4b4e2a56ec960
mallikarjunad <mallikarjuna.doddamane@manipalglobal.com>
2017-12-31 07:09:43 +0530

    Coursera-R Programming-Assignment
    It is a final version

---
+++ Coursera-R Prgramming-Assignment.R
@@ -0,0 +1,31 @@
+makeCacheMatrix <- function(x = matrix()) {
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+CacheSolve <- function(x, ...) {
+  i <- x$getinverse()
+  if (!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
+}
+###Testing
+
+A <- matrix(c(1,2,3,4),2,2)
+Acache <- makeCacheMatrix(A)
+CacheSolve(Acache)
+
